752377c0d678cc782788f47c1993bdb027a27afa
Administrator <wangyguang@qq.com>
2015-09-09 15:44:19 +0800

    observe

--- src/main/java/com/wang/design/observer/Test.java
+++ src/main/java/com/wang/design/observer/Test.java
@@ -1,11 +1,14 @@
 package com.wang.design.observer;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * Created by Administrator on 2015/9/9.
  */
 public class Test {
-    public static void main(String[] args) {
-        Product p = new Product("电视机", 176);
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
+        Product p = new Product("name", 1000);
         NameObserver observer = new NameObserver();
         PriceObserver obs = new PriceObserver();
         p.addObserver(observer);
@@ -13,5 +16,12 @@ public class Test {
 //      p.removeObserver(obs);
         p.setName("wang");
         p.setPrice(199);
+
+        Class cls = Class.forName("com.wang.design.observer.Product");
+        Method mtd = cls.getMethod("setName", new Class[]{String.class});
+        Product o = (Product) cls.newInstance();
+        o.addObserver(observer);
+        mtd.invoke(o, new Object[]{"myName"});
+
     }
 }
